dec6d7c63811648db2834ec55b0f7b3b9130d7ba
Kadoshima hagihara <kadoshima_hagihara@crystal-tec.co.jp>
2026-01-23 17:03:31 +0900

    commit

---
+++ _build/TX_SYNC_PULSE_TEST/core/FirmwareMSC.cpp.d
@@ -0,0 +1,6 @@
+C:\Users\tp240\Documents\Research\research_advertising_energy_saving\_build\TX_SYNC_PULSE_TEST\core\FirmwareMSC.cpp.o: \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\FirmwareMSC.cpp \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\FirmwareMSC.h \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\USBMSC.h \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/include/soc/esp32/include/soc/soc_caps.h \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/qio_qspi/include/sdkconfig.h
---
+++ _build/TX_SYNC_PULSE_TEST/core/firmware_msc_fat.c.d
@@ -0,0 +1,4 @@
+C:\Users\tp240\Documents\Research\research_advertising_energy_saving\_build\TX_SYNC_PULSE_TEST\core\firmware_msc_fat.c.o: \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\firmware_msc_fat.c \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\firmware_msc_fat.h \
+ C:\Users\tp240\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/include/newlib/platform_include/stdio.h
